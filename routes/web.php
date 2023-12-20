10625d99a49c86c44a2aba47de6c78938ac5b903
rafleskristiyanto <sitiora19@gmail.com>
2023-12-20 07:25:53 +0700

    message

--- routes/web.php
+++ routes/web.php
@@ -35,3 +35,4 @@ Route::post('/addpost', [PostController::class, 'createPost']);
 Route::get('/edit-post/{post}', [PostController::class, 'showEditScreen']);
 Route::put('/edit-post/{post}', [PostController::class, 'updatePost']);
 Route::delete('/delete-post/{post}', [PostController::class, 'deletePost']);
+//buat
